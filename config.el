aa660713b609d423753702f400d0202b7ab7d19d
sailsxu <sailsxu@tencent.com>
2021-05-31 09:46:31 +0800

    init

---
+++ config.el
@@ -0,0 +1,138 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "sailsxu"
+      user-mail-address "sailsxu@qq.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+; (setq doom-font (font-spec :family "Fira Mono" :size 12))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;; (setq doom-theme 'doom-molokai)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+(setq evil-default-state 'emacs)
+
+
+(setq doom-modeline-height 2)
+(setq doom-modeline-icon nil)
+;; (set-face-attribute 'mode-line nil :family "Noto Sans" :height 100)
+;; (set-face-attribute 'mode-line-inactive nil :family "Noto Sans" :height 100)
+
+(when IS-MAC
+    (setq mac-command-modifier 'meta)
+    (setq mac-option-modifier 'none))
+
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;pp
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+;;
+(put 'customize-themes 'disabled nil)
+(setq doom-theme 'doom-vibrant)
+
+(add-load-path! "lisp")
+(require 'init-convert)
+(require 'init-cc)
+
+
+;; (pushnew! initial-frame-alist '(width . 120) '(height . 60))
+;; 设置窗口位置
+(defun my/set-initial-frame ()
+  (let* ((width-factor 0.50)
+         (height-factor 0.80)
+         (a-width (* (display-pixel-width) width-factor))
+         (a-height (* (display-pixel-height) height-factor))
+         (a-left (truncate (/ (- (display-pixel-width) a-width) 2)))
+         ;; (a-top (truncate (/ (- (display-pixel-height) a-height) 2)))
+         )
+    ;; (set-frame-position (selected-frame) a-left a-top)
+    (set-frame-position (selected-frame) a-left 0)
+    (set-frame-size (selected-frame) (truncate a-width)  (truncate a-height) t)))
+(setq frame-resize-pixelwise t)
+(my/set-initial-frame)
+
+
+;; (global-set-key [remap switch-to-buffer] 'consult-buffer)
+(global-set-key [remap goto-line] 'consult-goto-line)
+;; (global-set-key (kbd "C-x b") 'consult-buffer)
+;;; :completion ivy
+(after! ivy
+  (global-set-key (kbd "C-x b") '+ivy/switch-buffer)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-virtual-abbreviate 'abbreviate))
+
+
+(setq ns-use-proxy-icon nil)
+(setq frame-title-format
+      '((:eval (if (buffer-file-name)
+                   (abbreviate-file-name (buffer-file-name))
+                 "%b"))))
+
+;; 高效的选中region
+(global-set-key (kbd "C-x m") 'er/expand-region)
+
+;; 复制当前buffer name
+(defun copy-file-name(choice)
+  "Copy the `buffer-file-name` to the `kill-ring` as CHOICE."
+  (interactive "cCopy Buffer Name (f) full, (d) directory, (n) name")
+  (let ((new-kill-string)
+        (name (if (eq major-mode 'dired-mode)
+                  (dired-get-filename)
+                (or (buffer-file-name) ""))))
+    (cond ((eq choice ?f)
+           (setq new-kill-string name))
+          ((eq choice ?d)
+           (setq new-kill-string (file-name-directory name)))
+          ((eq choice ?n)
+           (setq new-kill-string (file-name-nondirectory name)))
+          (t (message "Quit")))
+    (when new-kill-string
+      (message "%s copied" new-kill-string)
+      (kill-new new-kill-string)
+      )
+    )
+  )
+
+
+;; firestarter
+(firestarter-mode)
+
---
+++ lisp/init-cc.el
@@ -0,0 +1,71 @@
+;;; lisp/init-cc.el -*- lexical-binding: t; -*-
+
+(setq enable-local-variables t)
+(defvar +ccls-initial-blacklist [])
+(defvar +ccls-initial-whitelist [])
+;; cc-mode设置
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (google-set-c-style)
+
+            (setenv "GTAGSFORCECPP" "1")
+            (add-hook 'c-mode-hook 'helm-gtags-mode)
+            (add-hook 'c++-mode-hook 'helm-gtags-mode)
+            (add-hook 'protobuf-mode-hook 'helm-gtags-mode)
+            (with-eval-after-load 'helm-gtags
+              (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
+              (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-find-tag-from-here)
+              (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
+              (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack))
+
+            (require 'ccls)
+            (setq ccls-initialization-options
+                  `(:clang ,(list :extraArgs ["-isystem/Library/Developer/CommandLineTools/usr/include/c++/v1"
+                                              "-isystem/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+                                              "-isystem/usr/local/include"]
+                                  :resourceDir (cdr (doom-call-process "clang" "-print-resource-dir")))))
+
+
+            (add-hook 'lsp-mode-hook (lambda ()
+                      ;; 顶部目录、文件、方法breadcrumb
+                      (setq lsp-headerline-breadcrumb-enable nil)
+                      (setq lsp-modeline-diagnostics-enable nil)
+
+                      (lsp-diagnostics-mode -1)
+                      (flycheck-mode -1)
+                      ))
+            (add-hook 'hack-local-variables-hook
+                      (lambda ()
+                        (when (derived-mode-p 'c++-mode)
+                          ;; +ccls-initial-blacklist +ccls-initial-whitelist在dir-locals.el重新中设置新值
+                          (setq ccls-initialization-options
+                                (append ccls-initialization-options
+                                        `(:index (:threads 1 :initialBlacklist ,+ccls-initial-blacklist :initialWhitelist ,+ccls-initial-whitelist))))
+                          ;; (print ccls-initialization-options)
+                          (setq lsp-enable-file-watchers nil)
+                          (setq lsp-diagnostics-provider :none)
+                          (lsp)
+                          )
+                        ))
+
+            ;; 可以很方便的在头文件与cpp文件中切换
+            (setq cc-other-file-alist
+                  '(("\\.cc\\'"  (".hh" ".h")) ("\\.hh\\'"  (".cc" ".C"))
+                    ("\\.c\\'"   (".h")) ("\\.h\\'"   (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m" ".mm"))
+                    ("\\.m\\'"    (".h")) ("\\.mm\\'"    (".h"))
+                    ("\\.C\\'"   (".H"  ".hh" ".h")) ("\\.H\\'"   (".C"  ".CC"))
+                    ("\\.CC\\'"  (".HH" ".H"  ".hh" ".h")) ("\\.HH\\'"  (".CC"))
+                    ("\\.c\\+\\+\\'" (".h++" ".hh" ".h")) ("\\.h\\+\\+\\'" (".c++"))
+                    ("\\.cpp\\'" (".hpp" ".hh" ".h" "_p.h")) ("\\.hpp\\'" (".cpp"))
+                    ("\\.cxx\\'" (".hxx" ".hh" ".h")) ("\\.hxx\\'" (".cxx"))))
+            (setq ff-search-directories '("." "../src" "../include"))
+
+            (local-set-key  (kbd "M-o") 'ff-find-other-file)
+            )
+          )
+
+
+(provide 'init-cc)
+
+;;; init-cc.el ends here
